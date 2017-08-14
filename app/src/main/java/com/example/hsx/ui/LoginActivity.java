63a66fa7ef9d16fdbc5759577203c102898865bf
fjfhccfkuk <fjfhccfkuk@163.com>
2017-08-14 14:39:46 +0800

    添加login 界面

---
+++ app/src/main/java/com/example/hsx/ui/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.hsx.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.example.hsx.myapplication.R;
+
+/**
+ * Created by hsx on 17-8-14.
+ */
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.login_layout);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
